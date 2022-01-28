01dca44f48037be277b0f05e170bbfb754f0f233
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-01-28 11:47:19 +0100

    small change test integration tools

--- src/CalculusTreeTools.jl
+++ src/CalculusTreeTools.jl
@@ -216,4 +216,5 @@ export  is_constant, is_linear, is_quadratic, is_cubic, is_more_than_quadratic
 export transform_to_Expr, transform_to_expr_tree
 export delete_imbricated_plus, get_type_tree, get_elemental_variable, element_fun_from_N_to_Ni, cast_type_of_constant!
 export evaluate_expr_tree, calcul_gradient_expr_tree, calcul_Hessian_expr_tree
+
 end # module
