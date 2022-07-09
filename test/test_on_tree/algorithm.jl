e6e96fc90ea44daa9c6a59d56fd41f6d8233c8d4
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-07-09 14:04:53 -0300

    fix tests

--- test/test_on_tree/algorithm.jl
+++ test/test_on_tree/algorithm.jl
@@ -1,8 +1,10 @@
-@testset "" begin
+using ..ExpressionTreeForge.M_trait_expr_tree
+
+@testset "Algorithm tests" begin
   m = Model()
-  n = 5
+  n = 4
   @variable(m, x[1:n])
-  @NLobjective(m, Min, (x[1]*x[3]+5)^2 + x[2] + x[3] + x[4])
+  @NLobjective(m, Min, (x[1]*x[3]+5)^2 + sin(x[2]+cos(x[3])) + (-1 + x[4])^2)
 
   evaluator = JuMP.NLPEvaluator(m)
   MathOptInterface.initialize(evaluator, [:ExprGraph, :Hess])
@@ -11,7 +13,14 @@
   Expr_j = MathOptInterface.objective_expr(evaluator)
   expr_tree = ExpressionTreeForge.transform_to_expr_tree(Expr_j)
   expr_tree_j = copy(expr_tree)
+  @test ExpressionTreeForge.M_trait_expr_tree.expr_tree_equal(expr_tree, expr_tree_j)
+
   complete_tree = ExpressionTreeForge.create_complete_tree(expr_tree_j)
 
+  element_functions = delete_imbricated_plus(expr_tree_j)
+  element_variables = get_elemental_variable.(element_functions)
+
+  map((fun, var) -> element_fun_from_N_to_Ni!(fun, var), element_functions, element_variables)
+  @test ExpressionTreeForge.M_trait_expr_tree.expr_tree_equal(expr_tree, expr_tree_j) == false
 end
 
