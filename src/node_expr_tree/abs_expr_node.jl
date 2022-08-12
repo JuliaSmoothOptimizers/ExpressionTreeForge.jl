a3487171dad7744a2d7fd87dad37c0e3112f60bd
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-08-12 10:49:42 -0300

    improve abs_expr_node coverage

--- src/node_expr_tree/abs_expr_node.jl
+++ src/node_expr_tree/abs_expr_node.jl
@@ -6,7 +6,9 @@ import Base.^
 import Base.sin, Base.tan, Base.cos, Base.exp
 
 export Abstract_expr_node, create_node_expr
-export MyRef, new_ref, create_new_vector_myRef, set_myRef!, get_myRef
+export MyRef, new_ref, set_myRef!, get_myRef
+export create_new_vector_myRef, create_undef_array_myRef, create_vector_of_vector_myRef
+export equalize_vec_vec_myRef!
 
 """ Supertype of every node. """
 abstract type Abstract_expr_node end
@@ -107,7 +109,6 @@ Get the `value` of the `reference`.
 @inline *(ref1::MyRef{Y}, ref2::MyRef{Y}) where {Y <: Number} = get_myRef(ref1) * get_myRef(ref2)::Y
 @inline *(value::Y, ref::MyRef{Y}) where {Y <: Number} = value * get_myRef(ref)::Y
 @inline *(ref::MyRef{Y}, value::Y) where {Y <: Number} = value * get_myRef(ref)::Y
-@inline *(ref::MyRef{Y}) where {Y <: Number} = get_myRef(ref)::Y
 
 @inline /(ref1::MyRef{Y}, ref2::MyRef{Y}) where {Y <: Number} = get_myRef(ref1) / get_myRef(ref2)::Y
 @inline /(value::Y, ref::MyRef{Y}) where {Y <: Number} = value / get_myRef(ref)::Y
