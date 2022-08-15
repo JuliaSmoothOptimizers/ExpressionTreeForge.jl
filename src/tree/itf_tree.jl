b8609d9398524b5a4191d523cd95adfef384e935
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-08-15 15:37:51 -0300

    fixs

--- src/tree/itf_tree.jl
+++ src/tree/itf_tree.jl
@@ -7,13 +7,13 @@ using ..M_abstract_tree
 
 Get the current `node` as a part of `tree`.
 """
-_get_node(tree::AbstractTree) = @error("Should not be called")
+_get_node(tree::AbstractTree) = error("Should not be called")
 
 """
     children = _get_children(tree)
 
 Get the `children` from the current node as part of `tree`.
 """
-_get_children(tree::AbstractTree) = @error("Should not be called")
+_get_children(tree::AbstractTree) = error("Should not be called")
 
 end
