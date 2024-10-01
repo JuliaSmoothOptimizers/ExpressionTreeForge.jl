a81db3a8c191f727d050dd09f35688091f2160b3
paraynaud <paul.raynaud66@hotmail.fr>
2024-10-01 19:03:32 -0200

    fix get_expression_tree + add isapprox() (small difference of values when the backend DAG is different ETF vs MOI)

--- src/NLPSupport/get_expr_tree.jl
+++ src/NLPSupport/get_expr_tree.jl
@@ -3,27 +3,32 @@ using Symbolics
 using NLPModelsJuMP, ADNLPModels
 
 """
-    expr = get_expression_tree(adnlp::MathOptNLPModel)
+    expr = get_expression_tree(monlp::MathOptNLPModel)
     expr = get_expression_tree(adnlp::ADNLPModel)
     expr = get_expression_tree(model::JuMP.Model)
+    expr = get_expression_tree(evaluator::MOI.Nonlinear.Evaluator)
 
-Return the objective function as a `Type_expr_tree` for: a `MathOptNLPModel`, a `ADNLPModel` or a `JuMP.Model`.
+Return the objective function as a `Type_expr_tree` for: a `MathOptNLPModel`, a `ADNLPModel`, a `JuMP.Model` or a `MOI.Nonlinear.Evaluator`.
 """
-get_expression_tree(nlp::MathOptNLPModel) = get_expression_tree(nlp.eval.model)
 function get_expression_tree(model::JuMP.Model)
-  evaluator = JuMP.NLPEvaluator(model)
-  MathOptInterface.initialize(evaluator, [:ExprGraph])
-  obj_Expr = MathOptInterface.objective_expr(evaluator)::Expr
-  expr_tree =
-    ExpressionTreeForge.transform_to_expr_tree(obj_Expr)::ExpressionTreeForge.Type_expr_tree
-  return expr_tree
+  nlp = JuMP.nonlinear_model(model; force=true)
+  evaluator = MOI.Nonlinear.Evaluator(nlp)
+  F = MOI.get(model, MOI.ObjectiveFunctionType())
+  MOI.get(model, MOI.ObjectiveFunction{F}())
+  if F <: MOI.ScalarNonlinearFunction
+      MOI.Nonlinear.set_objective(nlp, MOI.get(model, MOI.ObjectiveFunction{F}()))
+      # has_nonlinear = true
+  end
+  return get_expression_tree(evaluator)
 end
 
-function get_expression_tree(model::MathOptInterface.Nonlinear.Model)
-  evaluator = MathOptInterface.Nonlinear.Evaluator(model)
+get_expression_tree(nlp::MathOptNLPModel) = get_expression_tree(nlp.eval)
+get_expression_tree(model::MathOptInterface.Nonlinear.Model) = get_expression_tree(MathOptInterface.Nonlinear.Evaluator(model))
+
+function get_expression_tree(evaluator::MOI.Nonlinear.Evaluator)
   MathOptInterface.initialize(evaluator, [:ExprGraph])
   obj_Expr = MathOptInterface.objective_expr(evaluator)::Expr
-  expr_tree =
+  expr_tree = 
     ExpressionTreeForge.transform_to_expr_tree(obj_Expr)::ExpressionTreeForge.Type_expr_tree
   return expr_tree
 end
